d0820b36133250c4f9213336f701446997ed626f
Thomas Meyer <thomasmmeyer88@gmail.com>
2021-01-06 11:44:10 -0600

    allow cross origins

--- src/main/java/com/reddit/demo/controllers/UserController.java
+++ src/main/java/com/reddit/demo/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 @RequestMapping(path="/user")
 public class UserController {
